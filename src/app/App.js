2a9454314add693d12537678697b329c6415955d
char502 <char502@hotmail.com>
2019-04-03 11:47:42 +0100

    connection to api established and info diplaying on page

---
+++ src/app/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import AstronomyContainer from "./components/AstronomyContainer";
+
+const App = () => {
+  return <AstronomyContainer />;
+};
+
+export default App;
